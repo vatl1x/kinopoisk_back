17178554b60977bcb79173a2c85e1a5880703d2f
Вадим <vatl1x@MacBook-Air-de-Vadim.local>
2025-12-22 01:10:01 +0300

    search problem

--- api/watch/[kpId].js
+++ api/watch/[kpId].js
@@ -11,19 +11,20 @@ export default async function handler(req, res) {
             },
         });
 
-        if (data.status !== "success" || !data.data) {
-            return res.status(404).json({ error: "not_found_in_alloha" });
-        }
+        // if (data.status !== "success" || !data.data) {
+        //     return res.status(404).json({ error: "not_found_in_alloha" });
+        // }
 
-        const movie = data.data;
-        const list = Object.values(movie.translation_iframe || {});
-        const dub = list.find((t) => t.name?.includes("Дублирован")) || list[0];
-        const iframeUrl = dub?.iframe || movie.iframe;
+        // const movie = data.data;
+        // const list = Object.values(movie.translation_iframe || {});
+        // const dub = list.find((t) => t.name?.includes("Дублирован")) || list[0];
+        // const iframeUrl = dub?.iframe || movie.iframe;
 
-        if (!iframeUrl)
-            return res.status(404).json({ error: "no_iframe_found" });
+        // if (!iframeUrl)
+        //     return res.status(404).json({ error: "no_iframe_found" });
 
-        res.json({ kpId, title: movie.name, year: movie.year, iframeUrl });
+        // res.json({ kpId, title: movie.name, year: movie.year, iframeUrl });
+        res.json(data);
     } catch {
         res.status(500).json({ error: "internal_error" });
     }
